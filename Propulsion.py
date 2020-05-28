a67f3e68a35d129eb25beec1b94d319c8d276075
jlachterberg <j.l.achterberg@student.tudelft.nl>
2020-05-28 15:43:48 +0200

    added rocket equation to propulsion.py

--- Propulsion.py
+++ Propulsion.py
@@ -3,13 +3,32 @@
 import numpy as np
 from Astrodynamics import mu_earth, dV_EOL, dV_eighty_km, dV_circularisation, dV_maintenance, dV_120, dV_realignment
 
+# input parameters
+m_i = 20    # kg
+g0 = 9.81
+
+def Transfer_Time(m, I_sp, T, r, r0):
+    # time for transfer
+    g = 9.81
+    t = m * g * I_sp / T * (1 - np.exp(1 / (I_sp * g) * (np.sqrt(mu_earth/r) - np.sqrt(mu_earth/r0))))
+    return t
+
+
+def rocket_equation(delta_v, isp, m_i):
+
+    w = g0*isp
+    Mp = (1 - np.exp(-delta_v/w))*m_i
+    return Mp
+
+
 # Chemical propulsion (impulsive manoeuvres)
 # Only circularisation
+# mono hydra blabla propellant test 1
+delta_v = dV_circularisation
 
+rocket_equation()
 
 # All manoeuvres
-
-
 
 # Electric propulsion (non-impulsive manoeuvres)
 # - 80km corrections?
@@ -21,16 +40,4 @@ from Astrodynamics import mu_earth, dV_EOL, dV_eighty_km, dV_circularisation, dV
 m = 20
 
 
-def Transfer_Time(m, I_sp, T, r, r0):
-    # time for transfer
-    g = 9.81
-    t = m * g * I_sp / T * (1 - np.exp(1 / (I_sp * g) * (np.sqrt(mu_earth/r) - np.sqrt(mu_earth/r0))))
-    return t
-
-
-def rocket_equation(delta_v, isp, m_i):
-    g0 = 9.81
-    w = g0*isp
-    Mp = (1 - np.exp(-delta_v/w))*m_i
-    return Mp
 
