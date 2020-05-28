1bcbcd48bb39c38cf8d8c566c3e72d8af2781889
jlachterberg <j.l.achterberg@student.tudelft.nl>
2020-05-28 11:50:12 +0200

    added hohmann transfer to astrodynamics file

--- Astrodynamics.py
+++ Astrodynamics.py
@@ -9,25 +9,30 @@
 import numpy as np
 
 # Orbital parameters
-m_earth = 5.9722 * 10 ** 24
-G = 6.67 * 10 ** -11
-mu_earth = m_earth * G / (10 ** 9)
-r_earth = 6378.136
-r_GEO = 42164.136
-T_GEO = 2 * np.pi * np.sqrt(r_GEO ** 3 / mu_earth)
-omega_GEO = np.sqrt(mu_earth / (r_GEO ** 3))
-peri_GTO = 250 #altitude
-apo_GTO = 35786 #altitude
-a_GTO = (peri_GTO + apo_GTO) / 2 + r_earth
-T_GTO = 2 * np.pi * np.sqrt(a_GTO ** 3 / mu_earth)
-d = np.sqrt(r_GEO ** 2 * 3)
+m_earth = 5.9722 * 10 ** 24                             # kg
+G = 6.67 * 10 ** -11                                    #
+mu_earth = m_earth * G / (10 ** 9)                      #
+r_earth = 6378.136                                      # km
+r_GEO = 42164.136                                       # km
+T_GEO = 2 * np.pi * np.sqrt(r_GEO ** 3 / mu_earth)      # s
+omega_GEO = np.sqrt(mu_earth / (r_GEO ** 3))            # rad/s
+peri_GTO = 250      # altitude                          # km
+apo_GTO = 35786     # altitude                          # km
+a_GTO = (peri_GTO + apo_GTO) / 2 + r_earth              # km
+T_GTO = 2 * np.pi * np.sqrt(a_GTO ** 3 / mu_earth)      # s
+d = np.sqrt(r_GEO ** 2 * 3)                             # km
 
 
+def Hohmann(r_dep, r_tar, mu_earth, r_earth):
+    # This function calculates the total delta-v needed for a Hohmann transfer,
+    # knowing the initial orbit and the target orbit (r_dep and r_tar)
+    a_T = 0.5*(r_dep+r_tar)
+    Vc_dep = np.sqrt(mu_earth/(r_dep + r_earth))
+    Vc_tar = np.sqrt(mu_earth/(r_tar + r_earth))
+    dV_1 = np.sqrt(mu_earth*(2/(r_dep + r_earth) - 1/(a_T + r_earth)))-Vc_dep
+    dV_2 = np.sqrt(mu_earth*(2/(r_tar + r_earth) - 1/(a_T + r_earth)))-Vc_tar
 
-
-def Hohmann():
-    t = 0
-    return t
+    return abs(dV_1) + abs(dV_2)
 
 
 def Phase_Shift(shift, t_transfer, r_GEO, T_GEO, mu_earth):
@@ -38,7 +43,11 @@ def Phase_Shift(shift, t_transfer, r_GEO, T_GEO, mu_earth):
     return dT, da, dV
 
 
-circularisation_orbit = Hohmann()
+r_dep, r_tar = 35706, 35786
+eighty_km = Hohmann(r_dep, r_tar, mu_earth, r_earth)
+print(eighty_km)
+circularisation_orbit = Hohmann(r_dep=1, r_tar=2, mu_earth=398600, r_earth=6378)
+
 
 # phase shift towards 120 degrees apart
 shift_120 = 2/3 * np.pi - (3 * omega_GEO * T_GTO - 2 * np.pi)
