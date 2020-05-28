fc52abfdbd8a7c09bceec185c8bfc59f9d4926e9
Gytha11 <gythaflore@hotmail.com>
2020-05-28 11:32:14 +0200

    phase shift

--- Astrodynamics.py
+++ Astrodynamics.py
@@ -1,9 +1,24 @@
 # This script calculates the delta-v based on these transfers:
 # - circularisation
 # - 80km corrections
-# - End Of Life manoeuvre
+# - phase shift towards constellation
 # - realignment
 # - orbit maintenances
+# - End Of Life manoeuvre
+
+import numpy as np
+
+# Orbital parameters
+m_earth = 5.9722 * 10 ** 24
+G = 6.67 * 10 ** -11
+mu_earth = m_earth * G / (10 ** 9)
+r_GEO = 42164.136
+T_GEO = 2 * np.pi * np.sqrt(r_GEO ** 3 / mu_earth)
+peri_GTO = 250 #altitude
+apo_GTO = 35786 #altitude
+d = np.sqrt(r_GEO ** 2 * 3)
+omega_GEO = np.sqrt(mu_earth / (r_GEO ** 3))
+
 
 
 def Hohmann():
@@ -11,9 +26,17 @@ def Hohmann():
     return t
 
 
-def Phase_Shift():
-    a = 0
-    return a
+def Phase_Shift(shift, t_transfer, r_GEO, T_GEO, mu_earth):
+    dT = t_transfer * T_GEO
+    dpos = shift / (2 * np.pi * r_GEO) * 2 * np.pi
+    da = dpos * r_GEO / (3 * np.pi) * T_GEO / dT
+    dV = 2 * (np.sqrt(mu_earth * (2 / r_GEO - 1 / (r_GEO + da))) - np.sqrt(mu_earth / r_GEO))
+    return dV
 
 
 circularisation_orbit = Hohmann()
+
+# phase shift towards 120 degrees apart
+shift_120 = 2/3 * np.pi - (3 * )
+t_transfer_120 = 7
+dV_realignment
