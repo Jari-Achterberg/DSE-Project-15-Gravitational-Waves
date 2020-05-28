923241bdb39a07dad9d755e29c2ea6b52220bebb
jlachterberg <j.l.achterberg@student.tudelft.nl>
2020-05-28 11:08:17 +0200

    minor changes to astrodynamics file

--- Astrodynamics.py
+++ Astrodynamics.py
@@ -1,2 +1,19 @@
 # This script calculates the delta-v based on these transfers:
-# -
+# - circularisation
+# - 80km corrections
+# - End Of Life manoeuvre
+# - realignment
+# - orbit maintenance
+
+
+def Hohmann():
+    t = 0
+    return t
+
+
+def Phase_Shift():
+    a = 0
+    return a
+
+
+circularisation_orbit = Hohmann()
