d236800db66abab0c4fa264f9767f28bd1cea2c7
jlachterberg <j.l.achterberg@student.tudelft.nl>
2020-05-28 17:22:00 +0200

    initial transfer time was calculated

--- Propulsion.py
+++ Propulsion.py
@@ -10,7 +10,7 @@ g0 = 9.81
 
 def Transfer_Time(m, I_sp, T, r, r0):
     # time for transfer
-    g = g0
+    g = g0 / 1000
     t = m * g * I_sp / T * (1 - np.exp(1 / (I_sp * g) * (np.sqrt(mu_earth/r) - np.sqrt(mu_earth/r0))))
     return t
 
@@ -29,13 +29,14 @@ delta_v = dV_circularisation * 1000
 delta_v_2 = total_delta_v * 1000
 # all options considered from left to right: (6 options)
 # Hydros-C, Lunar Flashlight MiPS, Argomoon Hibrid MiPS, Green Hybdrid, GR-1, 1 N GPHP, EPSS 1CKK
-isps = [310, 190, 190, 215, 231, 204, 252]
+isps = [310, 190, 190, 215, 231, 204, 252, 800]
 Mp_list, Mp_list_2 = [], []
 for isp in isps:
     Mp = rocket_equation(delta_v, isp, m_i)
     Mp_2 = rocket_equation(delta_v_2, isp, m_i)
     Mp_list.append(Mp)
     Mp_list_2.append(Mp_2)
+
 print(Mp_list)
 print(Mp_list_2)
 # All manoeuvres
@@ -47,7 +48,21 @@ print(Mp_list_2)
 # - orbit maintenances
 # - End Of Life manoeuvre?
 
-m = 20
+m = m_i
+isps = isps
+T = [1.2, 0.4, 0.1, 8, 0.26, 0.25, 1, 0.7/1000]
+r = 42164
+r0 = 250 + 6378
+
+t_list = []
+
+for i in range(len(isps)):
+    t = Transfer_Time(m, isps[i], T[i] / 1000, r, r0)
+    t /= 60
+    t /= 60
+    t /= 24
+    t_list.append(t)
 
 
+print("transfer times: ", t_list)
 
