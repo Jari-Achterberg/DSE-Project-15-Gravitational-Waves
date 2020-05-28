d11bd054fd3c1496c3baf8f4fbed45553d0a9d0c
Gytha11 <gythaflore@hotmail.com>
2020-05-28 12:43:16 +0200

    propulsion

--- Propulsion.py
+++ Propulsion.py
@@ -1,19 +1,28 @@
 # Determine what type of propulsion system is suitable for which manoeuvre
 
 import numpy as np
+from Astrodynamics import mu_earth
 
 # Chemical propulsion (impulsive manoeuvres)
-# - circularisation
-# - phase shift towards constellation
-# - realignment
-# - other manoeuvres that are not possible with electric
+# Only circularisation
+
+
+# All manoeuvres
+
+
 
 # Electric propulsion (non-impulsive manoeuvres)
 # - 80km corrections?
+# - phase shift towards constellation?
 # - realignment?
-# - orbit maintenances?
+# - orbit maintenances
 # - End Of Life manoeuvre?
+
+m = 20
 
 # time for transfer
 def Transfer_Time(m, I_sp, T, r, r0)
-t = m * g * I_sp / T * (1 - np.exp(1 / (I_sp * g)))
+    g = 9.81
+    t = m * g * I_sp / T * (1 - np.exp(1 / (I_sp * g) * (np.sqrt(mu_earth/r) - np.sqrt(mu_earth/r0))))
+    return t
+
